e80ea8f0aca39e6449cdac0a6b656b45f1db85cc
aprilhartley <77832842+aprilhartley@users.noreply.github.com>
2022-07-05 15:49:18 +0100

    Update ALSPAC_mums_GWAS_template.sh
    updated to BC4/ VCF files

--- ALSPAC_mums_GWAS_template.sh
+++ ALSPAC_mums_GWAS_template.sh
@@ -1,14 +1,20 @@
-#!/bin/bash
-#
-#PBS -l walltime=24:00:00
-#
-module add apps/snptest.2.5.0
-snptest_v2.5 -data /panfs/panasas01/shared/alspac/studies/latest/alspac/genetic/variants/arrays/gwas/imputed/1000genomes/released/27Feb2015/data/genotypes/dosage/data_chr0template.gz \
+#!/bin/sh
+#SBATCH --job-name=survival_gwas_chrtemplate
+#SBATCH --time=3-00:00:00
+#SBATCH -e GWAS_errorCtemplate
+#SBATCH --ntasks-per-node=14
+#SBATCH --nodes=1
+#SBATCH -p mrcieu,mrcieu2
+
+module add apps/snptest/2.5.2
+snptest_v2.5 -data /mnt/storage/private/alspac/1/alspac/studies/latest/alspac/genetic/variants/arrays/gwas/imputed/hrc/released/2017-05-04/data/vcf/data_chr0template.dose.vcf.gz \
                    PATH_TO/MOTHERS.sample \
-             -exclude_samples /panfs/panasas01/shared/alspac/deprecated/alspac_combined_1kg_20140424/docs/Unrelateds/MOTHER_UNRELATED_SNPTEST.txt \
-             -assume_chromosome template \
-             -frequentist 1 \
+	     -genotype_field GP \ #need to specify the field to read the genotypes from for VCF files
+             -exclude_samples /mnt/storage/private/alspac/1/alspac/studies/latest/alspac/genetic/variants/arrays/gwas/imputed/hrc/released/2015-10-30/data/derived/unrelated_ids/mothers/exclusion_list.txt \
+             	# or /mnt/storage/private/alspac/1/alspac/studies/latest/alspac/genetic/variants/arrays/gwas/imputed/hrc/released/2015-10-30/data/derived/unrelated_ids/children/exclusion_list.txt for children
+	     -frequentist 1 \
              -method expected \
+	     -use_raw_phenotypes \ #if you don't want the phenotype scaled to mean of 0 and variance of 1
 	     -pheno PHENO \
              -cov_names COVARS \
              -sex_column sex \
